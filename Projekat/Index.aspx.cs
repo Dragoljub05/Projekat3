261162cd206e6480a4ff1a23d65f461b46a9299e
esbz-ucenik03 <esbz-ucenik03@ESBZCOMP007>
2024-04-15 12:42:06 +0200

    Add project files.

---
+++ Projekat/Index.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Projekat
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cs = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=KorisnickiPodaci;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection con = new SqlConnection(cs);
+
+            string adm = Session["Provera"].ToString();
+
+            if (adm == "admin@gmail.com")
+            {
+                using(con)
+                {
+                    con.Open();
+                    Button1.Visible = true;
+                    TextBox1.Visible= true;
+                    TextBox2.Visible = true;
+                    TextBox3.Visible = true;
+                    string query = "SELECT * FROM Auto";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adp.Fill(dt);
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                    con.Close();
+                }
+               
+            }
+            else if (adm != "admin@gmail.com")
+            {
+                using(con)
+                {
+                    con.Open() ;
+                    string query = "SELECT * FROM Automobili";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adp.Fill(dt);
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                    con.Close();
+                }
+            }
+
+            else if(adm == "")
+            {
+                Response.Redirect("~/Register");
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string cs = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=dJ;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            using (con)
+            {
+                string query = "INSET INTO Auto VALUES('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "')";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                string query1 = "SELECT * FROM Auto";
+                SqlCommand cmd1 = new SqlCommand(query1, con);
+                SqlDataAdapter adp = new SqlDataAdapter(cmd1);
+                DataTable dt = new DataTable();
+                adp.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
---
+++ Projekat/Register.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Projekat
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session["Provera"] = "";
+        }
+
+   
+        protected void Registerdugme_Click(object sender, EventArgs e)
+        {
+            if(Page.IsValid) { 
+            string cs= "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=dJ;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                   string req = TextBox3.Text;
+                    if (TextBox3.Text == TextBox4.Text && req.Length > 5)
+                    {
+                        string query = "INSERT INTO Korisnici VALUES ('"+TextBox1.Text+ "','"+TextBox2.Text+"','"+TextBox3.Text+"')";
+                        SqlCommand cmd = new SqlCommand(query, con);
+                        try
+                        {
+                            con.Open();
+                            int uspeh = cmd.ExecuteNonQuery();
+                            if (uspeh > 0)
+                            {
+
+                                ErrorLabel.Text = "Uspesno prijavljeni!";
+                                Response.Redirect("~/Login.aspx");
+    
+                            }
+                            else
+                            {
+
+                                ErrorLabel.Text = "Nije uspelo!";
+
+                            }
+
+                        }
+
+                        catch (Exception ex)
+                        {
+                            
+                            ErrorLabel.Text = ex.ToString();
+
+                        }
+                    }
+                    else
+                    {
+                        ErrorLabel.Text = "Sifre se ne poklapaju!";
+                        return;
+                    }
+                }
+            }
+        }
+
+       
+    }
+}
